09bcd7be308642082b7ce4748492a671064c9d93
Alex Taffe <alex.taffe@gmail.com>
2018-02-11 17:40:31 -0500

    Create basic socket connections

---
+++ Client/AR Chess/AR Chess/RealTimeConnection.swift
@@ -0,0 +1,29 @@
+//
+//  RealtimeConnection.swift
+//  AR Chess
+//
+//  Created by Alex Taffe on 2/11/18.
+//  Copyright © 2018 Alex Taffe. All rights reserved.
+//
+
+import UIKit
+import Socket
+
+class RealTimeConnection: NSObject {
+    var socket:Socket?
+    
+    override init() {
+        do{
+            self.socket = try Socket.create()
+            try self.socket!.connect(to: "129.21.92.166", port: 4243, timeout: 0)
+            print("Connected")
+            while(true){
+                let result = try self.socket!.readString()
+                print("\(String(describing: result))")
+            }
+        }
+        catch{
+            
+        }
+    }
+}
